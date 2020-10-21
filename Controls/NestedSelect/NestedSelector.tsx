712b6e7c81a708d1262966bf3c633ea1b0058beb
Joe Royston <JRoyston@avtex.com>
2020-10-21 15:18:13 -0400

    added npm packages

--- Controls/NestedSelect/NestedSelector.tsx
+++ Controls/NestedSelect/NestedSelector.tsx
@@ -8,6 +8,11 @@ import { useBoolean, useConst } from '@uifabric/react-hooks';
 import { createListItems, createGroups, IExampleItem } from '@uifabric/example-data';
 import { IInputs } from './generated/ManifestTypes';
 import { ISelection } from '@fluentui/react';
+import { makeStyles } from '@material-ui/core/styles';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
 
 declare var Xrm: any;
 
